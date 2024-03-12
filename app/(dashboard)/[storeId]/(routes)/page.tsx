4124eadc1c3ba1416e212a412729ea9d70f11bbf
Aadarsh Kumaran <aadarshkumaranak@gmail.com>
2024-03-12 14:30:00 +0530

    Dashboard (Completed)

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,16 +1,85 @@
-import prismadb from "@/lib/prismadb";
+import { CreditCard, IndianRupeeIcon, PackageIcon } from "lucide-react";
 
-interface DashboardPageProps{
-    params:{storeId:string}
+import { Heading } from "@/components/ui/Heading";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
+import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { getSalesCount } from "@/actions/get-sales-count";
+import { getStockCount } from "@/actions/get-stock-count";
+import { Overview } from "@/components/overview";
+import { getRevenueGraph } from "@/actions/get-revenue-graph";
+
+interface DashboardPageProps {
+    params: { storeId: string }
 }
 
-const DashboardPage :React.FC<DashboardPageProps> = async ({params}) => {
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
     const store = await prismadb.store.findFirst({
-        where:{
+        where: {
             id: params.storeId
         }
     })
-    return <>Active Store {store?.name}</>
+
+    const totalRevenue = await getTotalRevenue(params.storeId)
+    const salesCount = await getSalesCount(params.storeId)
+    const stockCount = await getStockCount(params.storeId)
+    const revenueGraph = await getRevenueGraph(params.storeId)
+
+    return <>
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <Heading
+                    title="Dashboard" description={`Overview of ${store?.name}`}
+                />
+                <Separator />
+                <div className="grid gap-4 grid-cols-3">
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+                            <IndianRupeeIcon className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                {formatter.format(totalRevenue)}
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">Sales</CardTitle>
+                            <CreditCard className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                +{salesCount}
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">Products in Stock</CardTitle>
+                            <PackageIcon className="h-4 w-4 text-muted-foreground" />
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                {stockCount}
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+                <Card className="col-span-4">
+                    <CardHeader>
+                        <CardTitle>Overview</CardTitle>
+                    </CardHeader>
+                    <CardContent className="pl-2 ">
+                        <Overview data={revenueGraph}/>
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    </>
 }
 
 export default DashboardPage;
