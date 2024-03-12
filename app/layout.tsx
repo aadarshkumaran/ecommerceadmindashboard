46b21ce6c46e9f92474bf479ccf74ee337aa12d6
Aadarsh Kumaran <aadarshkumaranak@gmail.com>
2024-03-12 14:41:24 +0530

    Added Theme Changer.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ModalProvider } from "@/providers/modal-provider";
 import { ToasterProvider } from "@/providers/toast-provider";
 
 import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,9 +26,16 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          >
           <ToasterProvider/>
           <ModalProvider/>
           {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
